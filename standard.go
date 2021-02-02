05a376edbf9e02dafad82e87384e12e7e97085ba
Hayden Higginbotham <38926121+haydenhigg@users.noreply.github.com>
2021-02-02 10:18:11 -0500

    remove regularization & use []float64 for outputs

--- standard.go
+++ standard.go
@@ -1,18 +1,15 @@
 package percy
 
-func TrainFromWeights(initWeights []float64, inps [][]float64, outs []int, iters int, alpha float64) []float64 {
+func TrainFromWeights(initWeights []float64, inputs [][]float64, outputs []float64, iters int, alpha float64) []float64 {
 	var weights []float64
 
-	if n := len(inps); n == 0 {
+	if n := len(inputs); n == 0 {
 		return initWeights
 	} else {
 		weights = make([]float64, len(initWeights))
 
 		copy(weights, initWeights)
 	}
-
-	inputs := scaleEachToNorm(inps, 1)
-	outputs := intsToFloats(outs)
 
 	for iter := 0; iter < iters; iter++ {
 		for i, inp := range inputs {
