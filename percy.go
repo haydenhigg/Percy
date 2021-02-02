bd4d8f4f8be7b4c26645ed103f7716439580eb1f
Hayden Higginbotham <38926121+haydenhigg@users.noreply.github.com>
2021-02-02 10:16:55 -0500

    add regularize funcs & use []float64 for outputs

--- percy.go
+++ percy.go
@@ -1,26 +1,34 @@
 package percy
 
-func Train(inps [][]float64, outs []int, iters int, alpha float64) []float64 {
-	if n := len(inps); n == 0 {
+func Regularize(arr []float64) []float64 {
+	return scaleToNorm(arr, 1)
+}
+
+func RegularizeAll(mat [][]float64) [][]float64 {
+	return scaleEachToNorm(mat, 1)
+}
+
+func Train(inputs [][]float64, outputs []float64, iters int, alpha float64) []float64 {
+	if n := len(inputs); n == 0 {
 		return []float64{}
 	} else {
-		return TrainFromWeights(make([]float64, len(inps[0])), inps, outs, iters, alpha)
+		return TrainFromWeights(make([]float64, len(inputs[0])), inputs, outputs, iters, alpha)
 	}
 }
 
-func TrainAveraged(inps [][]float64, outs []int, iters int, alpha float64) []float64 {
-	if n := len(inps); n == 0 {
+func TrainAveraged(inputs [][]float64, outputs []float64, iters int, alpha float64) []float64 {
+	if n := len(inputs); n == 0 {
 		return []float64{}
 	} else {
-		return TrainAveragedFromWeights(make([]float64, len(inps[0])), inps, outs, iters, alpha)
+		return TrainAveragedFromWeights(make([]float64, len(inputs[0])), inputs, outputs, iters, alpha)
 	}
 }
 
 func RawPredict(weights, x []float64) float64 {
-	return dot(weights, scaleToNorm(x, 1))
+	return dot(weights, x)
 }
 
-func Predict(weights, x []float64) int {
+func Predict(weights, x []float64) float64 {
 	if RawPredict(weights, x) > 0 {
 		return 1
 	} else {
