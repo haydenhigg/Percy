6d95f73103487934656bdc55ab73fac4626dddfc
Hayden Higginbotham <38926121+haydenhigg@users.noreply.github.com>
2021-02-02 11:06:07 -0500

    change `alpha` to `learningRate`

--- standard.go
+++ standard.go
@@ -1,6 +1,6 @@
 package percy
 
-func TrainFromWeights(initWeights []float64, inputs [][]float64, outputs []float64, iters int, alpha float64) []float64 {
+func TrainFromWeights(initWeights []float64, inputs [][]float64, outputs []float64, iters int, learningRate float64) []float64 {
 	var weights []float64
 
 	if n := len(inputs); n == 0 {
@@ -17,7 +17,7 @@ func TrainFromWeights(initWeights []float64, inputs [][]float64, outputs []float
 
 			if dot(weights, inp) * out <= 0 { // if prediction and output do not match
 				for f := range weights {
-					weights[f] += inp[f] * out * alpha
+					weights[f] += inp[f] * out * learningRate
 				}
 			}
 		}
