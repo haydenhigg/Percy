68c2a9b468c10d1998446f7183132c860bd8628a
Hayden Higginbotham <38926121+haydenhigg@users.noreply.github.com>
2021-02-03 11:52:47 -0500

    move to `model`s + reorganization/clarification

--- averaged.go
+++ averaged.go
@@ -1,37 +1,42 @@
 package percy
 
-func TrainAveragedFromWeights(initWeights []float64, inputs [][]float64, outputs []float64, iters int, learningRate float64) []float64 {
-	var weights []float64
-	var averages []float64
-
+func TrainAveragedFromModel(init model, inputs [][]float64, outputs []float64, iters int, learningRate float64) model {
 	if n := len(inputs); n == 0 {
-		return initWeights
-	} else {
-		weights = make([]float64, len(initWeights))
-		averages = make([]float64, len(initWeights))
-
-		copy(weights, initWeights)
-		copy(averages, initWeights)
+		return init
 	}
+
+	weights := makeCopy(init.Weights)
+	averages := makeCopy(init.Weights)
+
+	bias := 0.
+	biasAverage := 0.
 
 	for iter := 0; iter < iters; iter++ {
 		for i, inp := range inputs {
 			out := outputs[i]
 
-			if dot(weights, inp) * out <= 0 { // if prediction and output do not match
-				for f, w := range weights {
-					modW := w + inp[f] * out * learningRate
-					
-					weights[f] = modW
-					averages[f] += modW
+			if (dot(weights, inp) + bias) * out <= 0 { // if prediction and output do not match
+				gradient := out * learningRate
+
+				for f := range weights {
+					weights[f] += inp[f] * gradient
+					averages[f] += weights[f]
 				}
+
+				bias += gradient
+				biasAverage += bias
 			} else {
 				for f, w := range weights {
 					averages[f] += w
 				}
+
+				biasAverage += bias
 			}
 		}
 	}
 
-	return averages
+	return model{
+		Weights: averages,
+		Bias: biasAverage,
+	}
 }
