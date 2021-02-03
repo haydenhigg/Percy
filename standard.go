435cdc8129dd3175414524341a26bf67273c2cc9
Hayden Higginbotham <38926121+haydenhigg@users.noreply.github.com>
2021-02-03 11:53:17 -0500

    move to `model`s + reorganization/clarification

--- standard.go
+++ standard.go
@@ -1,27 +1,32 @@
 package percy
 
-func TrainFromWeights(initWeights []float64, inputs [][]float64, outputs []float64, iters int, learningRate float64) []float64 {
-	var weights []float64
-
+func TrainFromModel(init model, inputs [][]float64, outputs []float64, iters int, learningRate float64) model {
 	if n := len(inputs); n == 0 {
-		return initWeights
-	} else {
-		weights = make([]float64, len(initWeights))
-
-		copy(weights, initWeights)
+		return init
 	}
+
+	weights := makeCopy(init.Weights)
+
+	bias := 0.
 
 	for iter := 0; iter < iters; iter++ {
 		for i, inp := range inputs {
 			out := outputs[i]
 
-			if dot(weights, inp) * out <= 0 { // if prediction and output do not match
+			if (dot(weights, inp) + bias) * out <= 0 { // if prediction and output do not match
+				gradient := out * learningRate
+
 				for f := range weights {
-					weights[f] += inp[f] * out * learningRate
+					weights[f] += inp[f] * gradient
 				}
+
+				bias += gradient
 			}
 		}
 	}
 
-	return weights
+	return model{
+		Weights: weights,
+		Bias: bias,
+	}
 }
